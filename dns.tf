a1b6af6fbec00e405a642ed1826e6f9c9737c45e
bethdevopsbunny <bethdevopsbunny@protonmail.com>
2023-08-02 17:29:20 +0100

    fix(dns): trailing dot for dns record and name

--- dns.tf
+++ dns.tf
@@ -1,14 +1,14 @@
-resource "google_dns_managed_zone" "zone" {
+resource "google_dns_managed_zone" "default" {
   name        = "iamjustintime-livelinklabs-delegation"
   dns_name    = var.dns_name
   description = "Delegated zone for iamjustintime application"
 }
 
-resource "google_dns_record_set" "frontend" {
-  name = google_dns_managed_zone.zone.dns_name
+resource "google_dns_record_set" "default" {
+  name = "${google_dns_managed_zone.default.dns_name}."
   type = "A"
   ttl  = 300
 
-  managed_zone = google_dns_managed_zone.zone.name
-  rrdatas = google_compute_global_address.default.address
+  managed_zone = google_dns_managed_zone.default.name
+  rrdatas      = [google_compute_global_address.default.address]
 }
